412b8739b97310a01aac551dcf54183674f3ae50
bjornlecis <bjorn.lecis@gmail.com>
2024-01-18 10:46:47 +0100

    Initial commit

---
+++ oefeningen.py
@@ -0,0 +1,12 @@
+import random
+getal = random.random() #getal tussen 0 en 1
+print(getal)
+#ik wil een getal tussen 1 en 100
+print(int(getal*100))
+#ik wil een getal tussen -50 en 50
+print(int(getal*100)-50)
+#ik wil een getal tussen 50 en 150
+print(int(getal*100)+50)
+print(random.randint(5,25))
+print(round(random.uniform(5.5,19.7),2))
+
